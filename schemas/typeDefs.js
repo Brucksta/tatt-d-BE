c70e8821d8c4ea0916f1bcd4823af0bab58c43ad
Peter Bruckner <peterbruckner08@gmail.com>
2021-08-16 14:45:48 +0930

    added typedefs/deleted package lock

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -40,7 +40,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!, artist: Boolean): Auth
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!, artist: Boolean, bio: String, style: String): Auth
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     login(email: String!, password: String!): Auth
     singleUpload(file: Upload!, tattooType: String!): File!
